f2b1a28b4dd10b1becff41606a766d2df283ed23
Bryan Rose <brose@degreed.com>
2022-03-10 13:55:59 -0600

    added metadata to copy_blob

--- target_snowflake/upload_clients/azure_blob_upload_client.py
+++ target_snowflake/upload_clients/azure_blob_upload_client.py
@@ -70,7 +70,7 @@ class AzureBlobUploadClient(BaseUploadClient):
         #TODO: - Update to capture metadata.  get_blob_metadata/set_blob_metadata
         #      - If archive container/prefix do not exist, create container/dir before copying
 
-        self.azure_client.copy_blob(container_name=prefixed_archive_container ,blob_name=source_key,copy_source='https://{}.blob.core.windows.net/{}'.format(az_account, copy_source))
+        self.azure_client.copy_blob(container_name=prefixed_archive_container ,blob_name=source_key,copy_source='https://{}.blob.core.windows.net/{}'.format(az_account, copy_source), metadata=archive_metadata)
         
         
         
